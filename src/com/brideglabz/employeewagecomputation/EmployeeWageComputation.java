859db945c2fe59cbd2d2bda0b120f8dd10927e23
usha-12 <usha12ahirwar@gmail.com>
2023-01-17 15:05:46 +0530

    Added: Introduced Part time employee work hours and wages

--- src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
@@ -2,12 +2,14 @@ package com.brideglabz.employeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
-        //Calculating Daily Wage of Employee
-        //wage per hour = 20
+        //Add Part time Employee & Wage
+        //Assume Part time Hour is 8
+        //wages per hour is 20
         int workingHours = 8;
         int dailyWage = workingHours * 20;
-        System.out.println("Daily Wage is : $" + dailyWage);
+        System.out.println("Daily Part Time Wage is : $" + dailyWage);
     }
     }
-    /*Calculate Daily
-Employee Wage - Assume Wage per Hour is 20 - Assume Full Day Hour is 8*/
+    /*Add Part time
+Employee & Wage
+- Assume Part time Hour is 8*/
