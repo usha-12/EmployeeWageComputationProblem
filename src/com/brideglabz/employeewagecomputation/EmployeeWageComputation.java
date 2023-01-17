3faaed7528f68dd3bb7420bcb06bb58dd111fbf1
usha-12 <usha12ahirwar@gmail.com>
2023-01-17 15:16:39 +0530

    Added: Calculating Wages for a Month

--- src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
@@ -8,6 +8,8 @@ public class EmployeeWageComputation {
         int empRatePerHr = 20;
         int isFullTime = 0;
         int isPartTime = 0;
+        int monthlyWage = 0;
+        int workingDays = 20;
         //Solving using switch case
         switch ((int)(empCheck)) {
 
@@ -23,16 +25,20 @@ public class EmployeeWageComputation {
                 workingHours = 8;
                 System.out.println("Employee is working Part Time");
                 System.out.println("Working hours of Employee are : " + workingHours);
-
                 break;
-
             default :
                 workingHours = 0;
                 break;
-
         }
+        //Calculating Wages for a Month
+        //Assume 20 Working Day per Month
+        dailyWage = workingHours * empRatePerHr;
+        monthlyWage = dailyWage * workingDays;
+
+        System.out.println("Daily Wage of Employee is : $" + dailyWage);
+        System.out.println("Monthly Wage of Employee is : $" + monthlyWage);
     }
     }
-    /*Solving using
-Switch Case
-Statement*/
+    /*Calculating Wages
+for a Month
+- Assume 20 Working Day per Month*/
