1c9be08dae5a90028b91c538ae7b7579e74b61dc
usha-12 <usha12ahirwar@gmail.com>
2023-01-17 15:11:17 +0530

    Added: Solving using

--- src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
@@ -2,14 +2,37 @@ package com.brideglabz.employeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
-        //Add Part time Employee & Wage
-        //Assume Part time Hour is 8
-        //wages per hour is 20
-        int workingHours = 8;
-        int dailyWage = workingHours * 20;
-        System.out.println("Daily Part Time Wage is : $" + dailyWage);
+        double empCheck = Math.floor(Math.random()*10) % 2;
+        int workingHours = 0;
+        int dailyWage = 0;
+        int empRatePerHr = 20;
+        int isFullTime = 0;
+        int isPartTime = 0;
+        //Solving using switch case
+        switch ((int)(empCheck)) {
+
+            case 1 :
+                isFullTime = 1;
+                workingHours = 8;
+                System.out.println("Employee is working Full Time");
+                System.out.println("Working hours of Employee are : " + workingHours);
+                break;
+
+            case 0 :
+                isPartTime = 0;
+                workingHours = 8;
+                System.out.println("Employee is working Part Time");
+                System.out.println("Working hours of Employee are : " + workingHours);
+
+                break;
+
+            default :
+                workingHours = 0;
+                break;
+
+        }
     }
     }
-    /*Add Part time
-Employee & Wage
-- Assume Part time Hour is 8*/
+    /*Solving using
+Switch Case
+Statement*/
