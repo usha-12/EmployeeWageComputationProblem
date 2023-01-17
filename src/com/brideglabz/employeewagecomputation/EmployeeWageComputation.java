4d072ed751bb662051d7b34ffdaea4172b9cb7b7
usha-12 <usha12ahirwar@gmail.com>
2023-01-17 14:57:54 +0530

    Added:Check Employee is Present or Absent

--- src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
@@ -1,2 +1,17 @@
-package com.brideglabz.employeewagecomputation;public class EmployeeWageComputation {
-}
+package com.brideglabz.employeewagecomputation;
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Problem");
+        //Check Employee is Present or Absent
+        double empCheck = Math.floor(Math.random()*10) % 2;
+        if (empCheck == 1)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+
+    }
+    }
+    /*Check Employee is
+Present or Absent
+- Use ((RANDOM)) for Attendance
+Check*/
