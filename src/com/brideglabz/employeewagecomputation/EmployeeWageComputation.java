444c1a83d3158a2af73128e1cad06560d99011bb
usha-12 <usha12ahirwar@gmail.com>
2023-01-18 22:25:07 +0530

    Added: Ability to compute Employee Wage for multiple companies

--- src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/brideglabz/employeewagecomputation/EmployeeWageComputation.java
@@ -1,53 +1,54 @@
 package com.brideglabz.employeewagecomputation;
 public class EmployeeWageComputation {
-        public static final int IS_FULL_TIME = 1;
-        public static final int IS_PART_TIME = 2;
-        public static final int EMP_RATE_PER_HOUR = 20;
-        public static final int NUM_OF_WORKING_DAYS = 20;
-        public static final int MAX_HRS_IN_MONTH = 100;
-
-        public static void main (String args[]){
-            System.out.println("Welcome to Employee Wage Computation Problem");
-            computeEmpWage();
-        }
-
-        public static void computeEmpWage () {
-            int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            int totalWorkingDays = 0;
-            int totalEmpHrs = 0;
-
-            for (totalWorkingDays = 1; totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS; totalWorkingDays++) {
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
-                switch (empCheck) {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        System.out.println("Employee is present Full time.");
-                        break;
-
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        System.out.println("Employee is present Part time.");
-                        break;
-
-                    default:
-                        empHrs = 0;
-                        System.out.println("Employee is absent.");
-                }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpHrs += empHrs;
-                //totalEmpWage += empWage;
-                System.out.println("Day " + totalWorkingDays + " Employee hours : " + empHrs + "	Wage $" + empWage);
-            }
-            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            System.out.println();
-            System.out.println("Employee Monthly Wage : $" + totalEmpWage);
-        }
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static void main(String args[]) {
+        computeEmpWage("Google", 50, 15, 200);
+        computeEmpWage("Amazon", 80, 20, 120);
+        computeEmpWage("Meta", 60, 20, 100);
+        computeEmpWage("Netflix", 90, 18, 220);
+        computeEmpWage("Tesla", 100, 20, 250);
     }
-/*Refactor the Code
-to write a Class
-Method to Compute
-Employee Wage - Use Class Method and Class
+
+    public static void computeEmpWage(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
+
+        for (totalWorkingDays = 1; totalEmpHrs <= maxHoursInMonth && totalWorkingDays < numOfWorkingDays; totalWorkingDays++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+            switch (empCheck) {
+                case IS_FULL_TIME :
+                    empHrs = 8;
+                    System.out.println("Employee is present Full time.");
+                    break;
+
+                case IS_PART_TIME :
+                    empHrs = 4;
+                    System.out.println("Employee is present Part time.");
+                    break;
+
+                default :
+                    empHrs = 0;
+                    System.out.println("Employee is absent.");
+            }
+            empWage = empHrs * empRatePerHour;
+            totalEmpHrs += empHrs;
+            //totalEmpWage += empWage;
+            System.out.println("Day " + totalWorkingDays + "	Employee hours : " + empHrs +"	Wage $" + empWage);
+        }
+        totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println();
+        System.out.println(companyName+" company Employee Monthly Wage : $" + totalEmpWage+"\n\n");
+    }
+    }
+/*Compute Employee Wage
+for multiple companies - Note: Each Company has its own
+wage, number of working days
+and working hours per month
+- Use Class Method with function
+parameters instead of Class
 Variables*/
